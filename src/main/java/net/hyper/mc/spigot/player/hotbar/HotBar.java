8bcd683e752bc7dd9f1f87f4e113caaafeea33a4
srbalbucio <srbalbuciogf@gmail.com>
2023-07-13 10:07:26 -0300

    Adicionado Player aos parametros do HotBar;

--- src/main/java/net/hyper/mc/spigot/player/hotbar/HotBar.java
+++ src/main/java/net/hyper/mc/spigot/player/hotbar/HotBar.java
@@ -1,11 +1,12 @@
 package net.hyper.mc.spigot.player.hotbar;
 
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
 
 public interface HotBar {
 
-    Map<Integer, ItemStack> getItems();
-    void clickEvent(ItemStack item, int slot);
+    Map<Integer, ItemStack> getItems(Player player);
+    void clickEvent(ItemStack item, int slot, Player player);
 }
